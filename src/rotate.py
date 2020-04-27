aa666ed17e8af5af0a61d3a812b93a2e5f1034d9
Kevin DeLand <kevin.deland@gmail.com>
2020-04-26 20:22:56 -0400

    attempt at rotation matrix

--- src/rotate.py
+++ src/rotate.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw
 from math import sin, cos, pi
 
 # Load image:
-input_image = Image.open("img/vaccines.png")
+input_image = Image.open("img/headscaled.png")
 input_pixels = input_image.load()
 
 # Create output image
@@ -19,13 +19,20 @@ center_y = input_image.height / 2
 ### I'm not sure I understand how this works or what it means...
 ### I suspect it has to do something with a matrix transformation?
 
+## I don't remember how this works... oh well
+A = [
+  [1, 1],
+  [1, 1]
+]
+### A * [cos(angle), sin(angle)]
+
 # Copy pixels
 for x in range(input_image.width):
   for y in range(input_image.height):
     # Compute coordinate in input image
-    xp = int( (x - center_x) * cos(angle) - (y - center_y) * sin(angle) + center_x)
-    yp = int( (x - center_x) * sin(angle) + (y - center_y) * cos(angle) + center_y)
+    xp = int( A[0][0]*(x - center_x) * cos(angle) +  A[0][1]*(y - center_y) * sin(angle) + center_x)
+    yp = int( A[1][1]*(x - center_x) * sin(angle) + A[1][0]*(y - center_y) * cos(angle) + center_y)
     if 0 <= xp < input_image.width and 0 <= yp < input_image.height:
       draw.point((x, y), input_pixels[xp, yp])
 
-output_image.save("img/rotated.png")
+output_image.save("img/rotated_2.png")
