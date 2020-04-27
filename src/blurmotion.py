ecdd253dc725bb619d3eb28c38711773d97e12b2
Kevin DeLand <kevin.deland@gmail.com>
2020-04-26 23:48:03 -0400

    motion blur: allow angle horizontal or vertical

--- src/blurmotion.py
+++ src/blurmotion.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw
 from numpy import dot
-from math import pi, cos, sin
+from math import pi, cos, sin, degrees
 
 # Load image
 ## input_image = Image.open("img/pittsburgh.jpg")
@@ -9,17 +9,20 @@ input_pixels = input_image.load()
 
 # Motion Blur kernel
 angle = pi /2 ## vertical
-## angle =  ## horizontal
-motion_size = 9 ## must be odd
+## angle = 0 ## horizontal
+motion_size = 3 ## must be odd
 motion_kernel = dot([1] * motion_size, 1/(motion_size))
 motion_kernel_angle = dot([
   [  0, sin(angle), 0],
   [cos(angle), 1, cos(angle)],
   [  0, sin(angle), 0]
 ], 1/3)
+
   
 # Select kernel here
-kernel = motion_kernel
+kernel = motion_kernel_angle
+print(kernel)
+print (len(kernel))
 
 # Middle of kernel
 offset = motion_size // 2
@@ -31,16 +34,18 @@ draw = ImageDraw.Draw(output_image)
 
 # Compute convolution (??) between intensity (??) and kernels
 for x in range(offset, input_image.width - offset):
-  for y in range(0, input_image.height):
+  for y in range(offset, input_image.height - offset):
     acc = [0, 0, 0] ## accumulator
     for a in range(len(kernel)): ## iterate through each cell within the kernel
-      xn = x + a - offset  ## this is the x,y value of the pixel we're observing
-      pixel = input_pixels[xn, y]
-      acc[0] += pixel[0] * kernel[a] ## red
-      acc[1] += pixel[1] * kernel[a] ## green
-      acc[2] += pixel[2] * kernel[a] ## blue
+      for b in range(len(kernel)):
+        xn = x + b - offset  ## this is the x,y value of the pixel we're observing
+        yn = y + a - offset
+        pixel = input_pixels[xn, yn]
+        acc[0] += pixel[0] * kernel[a][b] ## red
+        acc[1] += pixel[1] * kernel[a][b] ## green
+        acc[2] += pixel[2] * kernel[a][b] ## blue
 
     draw.point((x, y), (int(acc[0]), int(acc[1]), int(acc[2])))
 
-output_image.save("img/blur/motion/patchwork_" + str(motion_size) + ".png")
-## output_image.save("img/blur/motion/angle/patchwork_" + str(angle) + ".png")
+## output_image.save("img/blur/motion/patchwork_" + str(motion_size) + ".png")
+output_image.save("img/blur/motion/angle/patchwork_" + str(int(degrees(angle))) + ".png")
